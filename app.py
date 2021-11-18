ccc1aaf32deef8c2b29e530fc1f8e4e5d6ae5662
Brian Allen <briandouglasallen@gmail.com>
2021-11-18 14:03:47 -0500

    Further refinement of the figure description

--- app.py
+++ app.py
@@ -159,16 +159,19 @@ app.layout = html.Div(
             P("Many neurons fire a sequence of spikes, known as a burst, in response to stimuli. Waveforms of later spikes within a burst may be significantly less pronounced, making them harder to detect (or perhaps impossible to detect) with an electrode placed near the neuron. Bursting is presumed to play a role in neural coding, so understanding when we can and cannot detect burst spikes with a given technology could prove to be important."
               ),
             html.
-            P("This is an exploration of burst spikes as electrically sensed inside (intracellularly) and outside (extracellularly) of a single neuron, in an awake animal. The relatively high-fidelity intracellular signal acts as ground truth, telling us when to look for a spike from the particular neuron in the extracellular signal. The lower-fidelity extracellular signal will often contain spikes from other neurons in the vicinity, in addition to spikes from the neuron of interest (inferring which spikes originated from which neurons is the process known as spike sorting)."
+            P("This is an exploration of burst spikes as electrically sensed inside (intracellularly) and outside (extracellularly) of a single cortical neuron, in an awake animal. The relatively high-fidelity intracellular signal acts as ground truth, telling us when to look for a spike from the particular neuron in the extracellular signal. The lower-fidelity extracellular signal will often contain spikes from other neurons in the vicinity, in addition to spikes from the neuron of interest, making it difficult to tell which spikes originated from which neurons. This problem can be approached with the technique known as spike sorting."
               ),
             html.
-            P("Such recordings are typically difficult to achieve and are rare, affording a unique opportunity to assess the limits of spike detectability with a given electrode. These recordings were enabled by the technology developed as described in the paper linked to at the bottom of the page. Tap the data points in the figure (right) to explore spikes (left) that occur at various positions within a burst."
+            P("Recordings like these are typically difficult to achieve and are rare, affording a unique opportunity to assess the limits of spike detectability with a given electrode configuration and spike sorting algorithm. The recordings here were enabled by the technology developed as described in the paper linked to at the bottom of the page."
               ),
             html.
-            P("Left: Spikes as sensed intracellularly (top; the derivative of the signal with respect to time is in the middle), and extracellularly (bottom; filtered for spikes). Scalebar: 20ms (horiz.), 10mV/100\u03BCV (vert., top/bottom)"
+            P("Tap the data points in the figure (right) to explore spikes (left) that occur at various positions within a burst."
               ),
             html.
-            P("Right: Extracellular spike amplitude, colored by spike number within a burst, with respect to time since the previous spike (interspike interval). Key: non-burst spike (grey), 1st spike in burst (red), 2nd (green), 3rd (magenta), 4th (yellow), 5th (blue), 6th (orange). Tapping a datapoint will show its corresponding spike (left; the particular spike will be centered in the x-axis)"
+            P("Left: Spikes as sensed intracellularly (top; the derivative of the signal with respect to time is in the middle), and extracellularly (bottom; bandpass filtered for spikes). Scalebar: 20ms (horiz.), 10mV/100\u03BCV (vert., top/bottom)"
+              ),
+            html.
+            P("Right: Extracellular spike amplitude, colored by spike number within a burst, with respect to time since the previous spike (inter-spike interval). Key: Non-burst spike (grey), 1st spike in burst (red), 2nd (green), 3rd (magenta), 4th (yellow), 5th (blue), 6th (orange). Tapping a data point will show its corresponding spike (left; the particular spike will be centered in the x-axis)"
               ),
         ], style={'position': 'relative', 'margin-left': '40px', 'margin-right': '40px'}),
         dcc.Markdown(children=markdown_text, style={'position': 'relative', 'margin-left': '40px', 'margin-right': '40px'})
@@ -195,5 +198,5 @@ def display_image(clickData):
     return img_src
 
 if __name__ == '__main__':
-    #app.run_server(debug=False)
-    app.run_server(8001)
+    app.run_server(debug=False)
+    #app.run_server(8001)
